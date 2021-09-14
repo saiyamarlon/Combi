2aa8a07c04e9900b48be80b7d541ed472635971a
saiyamarlon <40893386+saiyamarlon@users.noreply.github.com>
2021-09-14 16:59:50 -0500

    ajuste clase 'elements'

--- Combinaatoria.py
+++ Combinaatoria.py
@@ -2,34 +2,31 @@ import itertools
 import random
 
 class elements():
+    def __init__(self):
+        self.listaP =[]
+        self.mirange = range(0,10)
 
-  def __init__(self):
-    self.listaC =[]
-    self.listaP =[]
+    def setRepete(self, number):
+        self.repete = number
 
-  def combinacion(self):
-    #print( list(itertools.combinations(range(0,10), 4)))
-    self.listaC =  list(itertools.combinations_with_replacement(range(0,10), 4))
+    def combinacion(self):
+        self.listaP =  list(itertools.combinations_with_replacement(self.mirange, self.repete))
 
-  def tomar(self):
-    pos = random.randrange(len(self.listaC))
-    return self.listaC[pos]
+    def tomar(self):
+        pos = random.randrange(len(self.listaP))
+        return self.listaP[pos]
 
-  def tomar1(self):
-    pos = random.randrange(len(self.listaP))
-    return self.listaP[pos]
-
-  def cartesiano(self, repete):
-    self.listaP = list(itertools.product(range(0,10),repeat = repete))
+    def cartesiano(self):
+        self.listaP = list(itertools.product(self.mirange, repeat = self.repete))
     
 
 
 if __name__ ==  '__main__':
-  gana = elements()
-  #gana.combinacion()
-  #print (gana.tomar1())
-  result  = gana.cartesiano(4)
-  #print(gana.listaP)
-  print (gana.tomar1())
-  result1  = gana.cartesiano(2)
-  print (gana.tomar1())
+  gana4numero = elements()
+  gana4numero.setRepete(4)
+  gana4numero.combinacion()
+  print (gana4numero.tomar())
+  gana4numero.setRepete(2)
+  gana4numero.cartesiano()
+  print (gana4numero.tomar())
+  
